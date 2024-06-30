0f0a7b911f46bce7744fcd1f77f89c8946132be7
Martian Jeirgif <mgj.exe@gmail.com>
2024-06-30 13:45:49 +0530

    Added DateTime module

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
+import DateTime from "./DateTime/DateTime.js";
 import RemoteHelloWorld from "./RemoteHelloWorld/RemoteHelloWorld.js";
 
-const INDEX = [RemoteHelloWorld];
+const INDEX = [DateTime, RemoteHelloWorld];
 
 export default INDEX;
