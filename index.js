154b19b8181a52229dbeb485ed1fdad1309b898c
M9J <mnsh.exe@gmail.com>
2024-07-01 01:15:53 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import RemoteHelloWorld from "./RemoteHelloWorld/RemoteHelloWorld.js";
 import DateTime from "./DateTime/DateTime.js";
 import Counter from "./Counter/Counter.js";
+import TestModule1 from "./TestModule1/TestModule1.js";
 
 const INDEX = [
+  TestModule1,
   Counter, 
   DateTime, 
   RemoteHelloWorld
