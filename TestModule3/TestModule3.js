bd50a6fec9b82d3aa9ff0bbc3a44707655009c44
Martian Jeirgif <mnsh.exe@gmail.com>
2024-07-02 00:18:39 +0530

    Delete TestModule3 directory

--- TestModule3/TestModule3.js
+++
@@ -1,6 +0,0 @@
-export default class TestModule3 {
-  MODULE_NAME = "Test Module 3";
-  MODULE_DESCRIPTION = "Test module 3 for checking CDN updates";
-  MODULE_VERSION = "0.2";
-  MODULE_MAIN = () => "Test Module 3 executed!"
-}
