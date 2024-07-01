61013f48938ea77f3d21e459fdda6dd3b480aa79
M9J <mnsh.exe@gmail.com>
2024-07-02 00:00:47 +0530

    update

---
+++ TestModule3/TestModule3.js
@@ -0,0 +1,6 @@
+export default class TestModule3 {
+  MODULE_NAME = "Test Module 3";
+  MODULE_DESCRIPTION = "Test module 3 for checking CDN updates";
+  MODULE_VERSION = "0.2";
+  MODULE_MAIN = () => "Test Module 3 executed!"
+}
--- index.js
+++ index.js
@@ -3,7 +3,8 @@ import DateTime from "./DateTime/DateTime.js";
 import Counter from "./Counter/Counter.js";
 import TestModule1 from "./TestModule1/TestModule1.js";
 import TestModule2 from "./TestModule2/TestModule2.js";
+import TestModule3 from "./TestModule3/TestModule3.js"
 
-const INDEX = [TestModule2, TestModule1, Counter, DateTime, RemoteHelloWorld];
+const INDEX = [TestModule3, TestModule2, TestModule1, Counter, DateTime, RemoteHelloWorld];
 
 export default INDEX;
