47696c1e203dbb204ccfc3c673a165cdeb2fb651
Martian Jeirgif <mgj.exe@gmail.com>
2024-07-01 22:05:35 +0530

    Added TestModule2

--- TestModule1/TestModule1.js
+++ TestModule1/TestModule1.js
@@ -1,6 +1,6 @@
 export default class TestModule1 {
   MODULE_NAME = "Test Module 1";
-  MODULE_DESCRIPTION = "Test module for checking CDN updates";
+  MODULE_DESCRIPTION = "Test module 1 for checking CDN updates";
   MODULE_VERSION = "0.1";
   MODULE_MAIN = () => "Test Module 1 executed!"
 }
--- index.js
+++ index.js
@@ -2,12 +2,8 @@ import RemoteHelloWorld from "./RemoteHelloWorld/RemoteHelloWorld.js";
 import DateTime from "./DateTime/DateTime.js";
 import Counter from "./Counter/Counter.js";
 import TestModule1 from "./TestModule1/TestModule1.js";
+import TestModule2 from "./TestModule2/TestModule2.js";
 
-const INDEX = [
-  TestModule1,
-  Counter, 
-  DateTime, 
-  RemoteHelloWorld
-];
+const INDEX = [TestModule2, TestModule1, Counter, DateTime, RemoteHelloWorld];
 
 export default INDEX;
