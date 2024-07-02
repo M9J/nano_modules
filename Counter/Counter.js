2780df04dd7fe6ccea51500dc2565005d3fd4c9d
Martian Jeirgif <mgj.exe@gmail.com>
2024-07-02 23:59:40 +0530

    Pulse updates to existing modules

--- Counter/Counter.js
+++ Counter/Counter.js
@@ -2,10 +2,11 @@ export default class Counter {
   MODULE_NAME = "Counter";
   MODULE_DESCRIPTION = "Counts from zero";
   MODULE_VERSION = "1.0";
-  MODULE_MAIN = (o) =>  {
+  MODULE_PULSE = true;
+  MODULE_MAIN = (o) => {
     let c = 0;
     setInterval(() => {
-      o(++c);
+      o.update(++c);
     }, 100);
     return c;
   };
--- DateTime/DateTime.js
+++ DateTime/DateTime.js
@@ -2,11 +2,12 @@ export default class DateTime {
   MODULE_NAME = "Today's Date and Time";
   MODULE_DESCRIPTION = "Prints and updates today's date and time";
   MODULE_VERSION = "0.1";
+  MODULE_PULSE = true;
   MODULE_MAIN = (o) => this.dateTime(o);
 
   dateTime(o) {
     setInterval(() => {
-      o(new Date().toLocaleString());
+      o.log(new Date().toLocaleString());
     }, 1000);
     return new Date().toLocaleString();
   }
