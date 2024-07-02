f79620b4675d69a119eea90de95e74876bf59815
M9J <mnsh.exe@gmail.com>
2024-07-03 02:09:29 +0530

    update TestModule1.js

--- TestModule1/TestModule1.js
+++ TestModule1/TestModule1.js
@@ -2,5 +2,5 @@ export default class TestModule1 {
   MODULE_NAME = "Test Module 1";
   MODULE_DESCRIPTION = "Test Module 1 module load test";
   MODULE_VERSION = "1.0";
-  MODULE_MAIN = () => "Testing complete.";
+  MODULE_MAINg = () => "Testing complete.";
 }
