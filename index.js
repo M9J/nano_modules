19d53f2e080ad7ada35d03931475247820fffb3c
M9J <mnsh.exe@gmail.com>
2024-07-04 02:03:16 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export default [
-  () => import("./Sender/Sender js"),
+  () => import("./Sender/Sender.js"),
   () => import("./Receiver/Receiver.js"),
   () => import("./Logger/Logger.js"),
   () => import("./TestModule1/TestModule1.js"),
