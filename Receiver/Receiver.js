ab0ed7bce4387d861165787807f821ff009a5612
Martian Jeirgif <mgj.exe@gmail.com>
2024-07-04 22:40:56 +0530

    Sender & Receiver updates

--- Receiver/Receiver.js
+++ Receiver/Receiver.js
@@ -2,5 +2,19 @@ export default class Receiver {
   MODULE_NAME = "Receiver Module 1";
   MODULE_DESCRIPTION = "This module receives message";
   MODULE_VERSION = "1.0";
-  MODULE_MAIN = () => "// TODO";
+  MODULE_CHANNEL_ID = "receiver@nanomodules.channel";
+
+  MODULE_MAIN = (o) => {
+    o.channel.onReceive(this.MODULE_CHANNEL_ID, ({ message, from }) => {
+      o.printLine(`${from}: ${message}`);
+      let tmr1 = setTimeout(() => {
+        clearTimeout(tmr1);
+        o.channel.send({
+          from: this.MODULE_CHANNEL_ID,
+          to: "sender@nanomodules.channel",
+          message: parseInt(message) + 1,
+        });
+      }, 1000);
+    });
+  };
 }
--- Sender/Sender.js
+++ Sender/Sender.js
@@ -2,5 +2,29 @@ export default class Sender {
   MODULE_NAME = "Sender Module 1";
   MODULE_DESCRIPTION = "This module sends message";
   MODULE_VERSION = "1.0";
-  MODULE_MAIN = () => "// TODO";
+  MODULE_CHANNEL_ID = "sender@nanomodules.channel";
+
+  MODULE_MAIN = (o) => {
+    o.channel.onReceive(this.MODULE_CHANNEL_ID, ({ message, from }) => {
+      o.printLine(`${from}: ${message}`);
+
+      let tmr1 = setTimeout(() => {
+        clearTimeout(tmr1);
+        o.channel.send({
+          from: this.MODULE_CHANNEL_ID,
+          to: "receiver@nanomodules.channel",
+          message: parseInt(message) + 1,
+        });
+      }, 1000);
+    });
+    let tmr = setTimeout(() => {
+      clearTimeout(tmr);
+      console.log("message sent");
+      o.channel.send({
+        from: this.MODULE_CHANNEL_ID,
+        to: "receiver@nanomodules.channel",
+        message: "1",
+      });
+    }, 1000);
+  };
 }
