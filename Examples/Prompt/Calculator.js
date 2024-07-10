1e09b3a47cb02d451817d71ae56f3ab5c8a0c596
Martian from Remote Planet <mnsh.exe@gmail.com>
2024-07-10 22:22:52 +0530

    update Calculator.js

---
+++ Examples/Prompt/Calculator.js
@@ -0,0 +1,25 @@
+export default class Calculator {
+  MODULE_NAME = "Calculator";
+  MODULE_DESCRIPTION = "A simple calculator";
+  MODULE_VERSION = "1.0";
+  MODULE_MAIN = () => {
+    const divMain = document.createElement("div");
+    const divRow1 = document.createElement("div");
+    const num1 = document.createElement("input");
+    num1.type = "text";
+    const num2 = document.createElement("input");
+    num2.type = "text";
+    const button = document.createElement("button");
+    button.onclick = () => {
+      value = num1.value + num2.value;
+      this.MODULE_OUTPUT.print(value);
+    }
+    divRow1.appendChild(num1);
+    divRow1.appendChild("+");
+    divRow1.appendChild(num2);
+    divMain.appendChild(divRow1);
+    dovMain.appendChild(button);
+    this.MODULE_OUTPUT.printElement(divMain);
+  };
+
+}
