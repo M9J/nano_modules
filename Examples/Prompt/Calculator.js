3020f41c2a5f72e02456b41bdbb22ecf3d209f3a
Martian from Remote Planet <mnsh.exe@gmail.com>
2024-07-12 00:24:46 +0530

    update Calculator.js

--- Examples/Prompt/Calculator.js
+++ Examples/Prompt/Calculator.js
@@ -3,27 +3,15 @@ export default class Calculator {
   MODULE_DESCRIPTION = "A simple calculator";
   MODULE_VERSION = "1.0";
   MODULE_MAIN = () => {
-    const divMain = document.createElement("div");
-    const divRow1 = document.createElement("div");
-    divRow1.style = "display: flex;"
-    const num1 = document.createElement("input");
-    num1.type = "text";
-    const plus = document.createElement("span");
-    plus.innerHTML = "+";
-    const num2 = document.createElement("input");
-    num2.type = "text";
-    const button = document.createElement("button");
-    button.innerHTML = "Calculate";
-    button.onclick = () => {
-      const value = +num1.value + +num2.value;
-      this.MODULE_OUTPUT.print(value);
-    }
-    divRow1.appendChild(num1);
-    divRow1.appendChild(plus);
-    divRow1.appendChild(num2);
-    divMain.appendChild(divRow1);
-    divMain.appendChild(button);
-    this.MODULE_OUTPUT.printElement(divMain);
+    this.calculate();
   };
+  
+  async calculate() {
+    const num1 = await this.MODULE_OUTPUT.prompt("Number 1", true);
+    const num2 = await this.MKDULE_OUTPUT.prompt("Number 2", true);
+    const result = +num1 + +num2;
+    this.MODULE_OUTPUT.print(`${num1} + ${num2} = ${result}`);
+    this.calculate();
+  }
 
 }
